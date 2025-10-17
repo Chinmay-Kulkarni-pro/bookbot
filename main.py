05011ee214c72c1c51a072b017d1e6dd99858710
Chinmay-Kulkarni-pro <chinmaykulkarni2208@gmail.com>
2025-10-17 14:26:37 +0530

    Almost completed

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def main():
 
     file_path = "books/frankenstein.txt"
 
-    num_words = num_of_words(file_path)
-    print(f"Found {num_words} total words")
-
-    num_chars = num_of_char(file_path)
-    print(num_chars)
-
     sorted_list = sorted_list_dict(file_path)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    num_words = num_of_words(file_path)
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_list:
+        if dict["char"].isalpha():
+            first = dict["char"]
+            second = dict["num"]
+            print(f"{first}: {second}")
+    
 main()
